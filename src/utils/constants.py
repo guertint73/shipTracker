ef04e7f8a442774fe8b4da242ffe136cddfcad3b
Timothy Guertin <guertint@msu.edu>
2022-07-15 18:26:22 -0500

    now runs in heroku

--- src/utils/constants.py
+++
@@ -1,9 +0,0 @@
-class Constants:
-    """Reusable constants."""
-
-    boatnerd_url = f'https://ais.boatnerd.com/vessel/view/all?bbox'\
-               f'=-96.152344 35.657296,-96.152344 50.583237,'\
-               f'-70.070801 50.583237,-70.070801 35.657296,'\
-               f'-96.152344 35.657296'
-
-    freighter = 'Freighter'
